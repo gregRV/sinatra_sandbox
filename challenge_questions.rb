c88cf412bce92e9118fafed7ac958ed11584ac31
gregRV <gregvarias@gmail.com>
2013-06-25 18:14:22 -0700

    Create challenge_questions.rb

---
+++ challenge_questions.rb
@@ -0,0 +1,9 @@
+#1. When would a form use a 'get' method?
+
+#2. Does every 'get' route have a pairing post route?
+
+#3. Is it common to have several forms on the same page?
+
+#4. What's the scope of instance variables in a route?
+
+#5. Can/should you access the params hash from outside index.rb?
